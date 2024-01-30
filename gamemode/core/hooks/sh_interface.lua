9e6d156f50451c10020445c53ea8231d73ad7b9a
TankNut <m.vlasblom@live.nl>
2024-01-30 20:40:12 +0100

    Move interface hooks into core

--- gamemode/core/hooks/sh_interface.lua
+++
@@ -1,9 +0,0 @@
-if SERVER then
-	function GM:ShowTeam(ply)
-		ply:OpenGroupedInterface("CharacterSelect", "F2")
-	end
-
-	function GM:ShowSpare1(ply)
-		ply:OpenGroupedInterface("PlayerMenu", "F3")
-	end
-end
--- gamemode/core/sh_interface.lua
+++ gamemode/core/sh_interface.lua
@@ -74,3 +74,13 @@ else
 		netstream.Send(self, "OpenInterface", {Name = name, Group = group, Args = {...}})
 	end
 end
+
+if SERVER then
+	function GM:ShowTeam(ply)
+		ply:OpenGroupedInterface("CharacterSelect", "F2")
+	end
+
+	function GM:ShowSpare1(ply)
+		ply:OpenGroupedInterface("PlayerMenu", "F3")
+	end
+end
