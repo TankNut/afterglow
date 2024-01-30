9f9c18217b6ee2bf9b102b4efd4f44c83d318219
TankNut <m.vlasblom@live.nl>
2024-01-30 20:48:28 +0100

    Move item hooks into core

--- gamemode/core/sh_item.lua
+++ gamemode/core/sh_item.lua
@@ -217,3 +217,28 @@ if SERVER then
 		PlayerCreate(ply, args[1])
 	end)
 end
+
+function GM:GetItemDropLocation(ply)
+	local tr = util.TraceLine({
+		start = ply:GetShootPos(),
+		endpos = ply:GetShootPos() + ply:GetAimVector() * 50,
+		filter = ply,
+		collisiongroup = COLLISION_GROUP_WEAPON
+	})
+
+	local ang = ply:GetAngles()
+
+	ang.p = 0
+
+	return tr.HitPos + tr.HitNormal * 10, ang
+end
+
+function GM:ItemEquipped(ply, item, loaded)
+	item:OnEquip()
+	item:FireEvent("EquipmentChanged")
+end
+
+function GM:ItemUnequipped(ply, item)
+	item:OnUnequip()
+	item:FireEvent("EquipmentChanged")
+end
