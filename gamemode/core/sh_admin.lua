aed79b339afcb6944c0dbc45bad0fd51c94afa4e
TankNut <m.vlasblom@live.nl>
2024-02-05 00:47:09 +0100

    Admin ranks

--- gamemode/core/sh_admin.lua
+++ gamemode/core/sh_admin.lua
@@ -1,3 +1,18 @@
+module("Admin", package.seeall)
+
+local meta = FindMetaTable("Player")
+
+Ranks = Ranks or {}
+
+function AddRank(name, inheritance)
+	Ranks[name:lower()] = inheritance and inheritance:lower() or true
+end
+
+AddRank("user")
+AddRank("admin", "user")
+AddRank("superadmin", "admin")
+AddRank("developer", "superadmin")
+
 PlayerVars.Register("UserGroup", {
 	Field = "usergroup",
 	Default = "user",
@@ -6,6 +21,40 @@ PlayerVars.Register("UserGroup", {
 		ply:SetUserGroup(new)
 	end
 })
+
+function meta:CheckUserGroup(group)
+	local usergroup = self:GetUserGroup()
+
+	if usergroup == group then
+		return true
+	end
+
+	while usergroup do
+		local parent = Ranks[usergroup]
+
+		if parent == group then
+			return true
+		elseif parent == true then
+			return false
+		end
+
+		usergroup = parent
+	end
+
+	return false
+end
+
+function meta:IsAdmin()
+	return self:CheckUserGroup("admin")
+end
+
+function meta:IsSuperAdmin()
+	return self:CheckUserGroup("superadmin")
+end
+
+function meta:IsDeveloper()
+	return self:CheckUserGroup("developer")
+end
 
 if SERVER then
 	hook.Remove("PlayerInitialSpawn", "PlayerAuthSpawn")
