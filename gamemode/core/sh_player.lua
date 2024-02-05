8a4a28af06d874413a95476ec9e291c1165a7762
TankNut <m.vlasblom@live.nl>
2024-02-05 01:14:56 +0100

    Appearance.Copy onto ragdolls

--- gamemode/core/sh_player.lua
+++ gamemode/core/sh_player.lua
@@ -3,3 +3,9 @@ function GM:SetupMove(ply, mv, cmd)
 		mv:SetMaxClientSpeed(Lerp(0.6, ply:GetWalkSpeed(), ply:GetRunSpeed()))
 	end
 end
+
+if CLIENT then
+	function GM:CreateClientsideRagdoll(ent, ragdoll)
+		Appearance.Copy(ent, ragdoll)
+	end
+end
