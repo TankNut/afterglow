3a24429b99582dd6e0af88cb28c0ddf7198bbc27
TankNut <m.vlasblom@live.nl>
2024-02-07 19:40:46 +0100

    Fix backwards crouching being fast

--- gamemode/core/sh_player.lua
+++ gamemode/core/sh_player.lua
@@ -1,6 +1,6 @@
 function GM:SetupMove(ply, mv, cmd)
 	if cmd:GetForwardMove() <= 0 then
-		mv:SetMaxClientSpeed(Lerp(0.6, ply:GetWalkSpeed(), ply:GetRunSpeed()))
+		mv:SetMaxClientSpeed(math.min(Lerp(0.6, ply:GetWalkSpeed(), ply:GetRunSpeed()), mv:GetMaxClientSpeed()))
 	end
 end
 
