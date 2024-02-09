75031a3fe10378bf0570a1e35fc797047db4925a
TankNut <m.vlasblom@live.nl>
2024-02-09 05:03:18 +0100

    Fix a stray global

--- gamemode/utils/extensions/sh_math.lua
+++ gamemode/utils/extensions/sh_math.lua
@@ -1,5 +1,5 @@
 function math.ApproachSpeed(start, dest, speed)
-	dist = math.max(math.abs(start - dest), 0.0001)
+	local dist = math.max(math.abs(start - dest), 0.0001)
 
 	return math.Approach(start, dest, dist / speed)
 end
