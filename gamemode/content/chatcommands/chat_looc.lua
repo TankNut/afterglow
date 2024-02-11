c4fe72f86d53838bad50c48ffecb67bdaee51216
TankNut <m.vlasblom@live.nl>
2024-02-11 01:24:57 +0100

    Local OOC

---
+++ gamemode/content/chatcommands/chat_looc.lua
@@ -0,0 +1,24 @@
+CLASS.Name = "Local OOC"
+CLASS.Description = "Local out-of-character chat."
+
+CLASS.Commands = {"looc"}
+CLASS.Aliases = {"[[", ".//"}
+
+CLASS.Range = 400
+
+CLASS.Tabs = TAB_LOOC
+
+CLASS.Color = Color(138, 185, 209)
+
+if CLIENT then
+	function CLASS:OnReceive(data)
+		return string.format("<c=%s>%s: [LOCAL-OOC] %s", self.Color, data.Name, data.Text)
+	end
+else
+	function CLASS:Parse(ply, lang, cmd, text)
+		return {
+			Name = ply:GetCharacterName(),
+			Text = text
+		}
+	end
+end
