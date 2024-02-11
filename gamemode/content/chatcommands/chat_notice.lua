c2a09a2fa3475446fb31d2a4be028af4fa2905cc
TankNut <m.vlasblom@live.nl>
2024-02-11 01:37:48 +0100

    Error/notice chat 'commands'

---
+++ gamemode/content/chatcommands/chat_notice.lua
@@ -0,0 +1,9 @@
+CLASS.Name = "NOTICE"
+
+CLASS.Color = Color(229, 201, 98)
+
+if CLIENT then
+	function CLASS:OnReceive(data, colors)
+		return string.format("<c=%s>%s", self.Color, data.Text)
+	end
+end
