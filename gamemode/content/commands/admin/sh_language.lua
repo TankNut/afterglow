65e2a22efbd22c206bf2d27fbed2929acc75d09b
TankNut <m.vlasblom@live.nl>
2024-02-14 06:40:17 +0100

    rpa_takelanguage

--- gamemode/content/commands/admin/sh_language.lua
+++ gamemode/content/commands/admin/sh_language.lua
@@ -15,7 +15,29 @@ local give = console.AddCommand("rpa_givelanguage", function(ply, targets, lang)
 	end
 end)
 
-give:SetDescription("Gives a player the ability to speak and hear a specific language.")
+give:SetDescription("Gives a character the ability to speak and hear a specific language.")
 give:AddParameter(console.Player())
 give:AddParameter(console.Language())
 give:SetAccess(Command.IsAdmin)
+
+local take = console.AddCommand("rpa_takelanguage", function(ply, targets, lang)
+	local name = Language.GetName(lang)
+
+	for _, target in pairs(targets) do
+		if not target:HasLanguage(lang) then
+			console.Feedback(ply, "ERROR", "%s doesn't speak %s.", target:GetCharacterName(), name)
+
+			continue
+		end
+
+		target:RemoveLanguage(lang)
+
+		console.Feedback(ply, "NOTICE", "You've taken the ability to speak %s from %s.", name, target:GetCharacterName())
+		console.Feedback(target, "NOTICE", "%s has removed your ability to speak %s.", ply, name)
+	end
+end)
+
+take:SetDescription("Take the ability to speak and hear a specific language from a character.")
+take:AddParameter(console.Player())
+take:AddParameter(console.Language())
+take:SetAccess(Command.IsAdmin)
