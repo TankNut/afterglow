0b39cabd81d9bb7c5eeafb934abe5b519c1c1226
TankNut <m.vlasblom@live.nl>
2024-02-14 06:40:30 +0100

    More language commands

--- gamemode/content/commands/admin/sh_language.lua
+++ gamemode/content/commands/admin/sh_language.lua
@@ -41,3 +41,82 @@ take:SetDescription("Take the ability to speak and hear a specific language from
 take:AddParameter(console.Player())
 take:AddParameter(console.Language())
 take:SetAccess(Command.IsAdmin)
+
+local reset = console.AddCommand("rpa_resetlanguages", function(ply, targets)
+	local cache = {}
+
+	for _, target in pairs(targets) do
+		target:SetLanguages(target:GetCharacterFlagAttribute("DefaultLanguages"))
+		target:CheckLanguage()
+
+		local flag = target:GetCharacterFlag()
+		local names = cache[flag]
+
+		if not names then
+			names = target:GetLanguages()
+
+			for k, lang in pairs(names) do
+				names[k] = Language.GetName(lang)
+			end
+
+			names = table.concat(names, ", ")
+			cache[flag] = names
+		end
+
+		if #names < 1 then
+			names = "none"
+		end
+
+		console.Feedback(ply, "NOTICE", "You've reset %s's languages to (%s).", target:GetCharacterName(), names)
+		console.Feedback(target, "NOTICE", "%s has reset your languages to (%s).", ply, names)
+	end
+end)
+
+reset:SetDescription("Resets a character's languages to the default for their character flag.")
+reset:AddParameter(console.Player())
+reset:SetAccess(Command.IsAdmin)
+
+local set = console.AddCommand("rpa_setlanguages", function(ply, targets, languages)
+	print(type(languages))
+	languages = string.Explode("[^%a]+", languages, true)
+
+	for k, lang in pairs(languages) do
+		if k == 1 and lang == "" then
+			languages = {}
+			break
+		end
+
+		if not Language.Get(lang) then
+			console.Feedback(ply, "ERROR", "Unknown language: %s", lang)
+
+			return
+		end
+	end
+
+	local names = {}
+
+	for k, lang in pairs(languages) do
+		names[k] = Language.GetName(lang)
+	end
+
+	names = table.concat(names, ", ")
+
+	if #names < 1 then
+		names = "none"
+	end
+
+	languages = table.Lookup(languages)
+
+	for _, target in pairs(targets) do
+		target:SetLanguages(languages)
+		target:CheckLanguage()
+
+		console.Feedback(ply, "NOTICE", "You've set %s's languages to (%s).", target:GetCharacterName(), names)
+		console.Feedback(target, "NOTICE", "%s has set your languages to (%s).", ply, names)
+	end
+end)
+
+set:SetDescription("Sets a character's languages to a specific set of languages.")
+set:AddParameter(console.Player())
+set:AddOptional(console.String({}, "Languages"), "", "none")
+set:SetAccess(Command.IsAdmin)
