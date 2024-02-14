d2b494f65122bb0c4ac5e7ee0344db87aab7ce98
TankNut <m.vlasblom@live.nl>
2024-02-14 06:04:43 +0100

    Properly implements rpa_givelanguage

--- gamemode/content/commands/admin/sh_language.lua
+++ gamemode/content/commands/admin/sh_language.lua
@@ -1,9 +1,21 @@
-local give = console.AddCommand("rpa_givelanguage", function(ply, delay)
-	timer.Simple(delay, function()
-		RunConsoleCommand("changelevel", game.GetMap())
-	end)
+local give = console.AddCommand("rpa_givelanguage", function(ply, targets, lang)
+	local name = Language.GetName(lang)
+
+	for _, target in pairs(targets) do
+		if target:HasLanguage(lang) then
+			console.Feedback(ply, "ERROR", "%s already speaks %s.", target:GetCharacterName(), name)
+
+			continue
+		end
+
+		target:AddLanguage(lang)
+
+		console.Feedback(ply, "NOTICE", "You've given %s the ability to speak %s.", target:GetCharacterName(), name)
+		console.Feedback(target, "NOTICE", "%s has given you the ability to speak %s.", ply, name)
+	end
 end)
 
-give:SetDescription("Restarts the current map after a delay.")
-give:AddOptional(console.Time({}, "Delay"), 5, "5 seconds")
+give:SetDescription("Gives a player the ability to speak and hear a specific language.")
+give:AddParameter(console.Player())
+give:AddParameter(console.Language())
 give:SetAccess(Command.IsAdmin)
