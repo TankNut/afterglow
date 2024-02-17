128c7c04e289aa7c3263fedd48f00ee8e2203685
TankNut <m.vlasblom@live.nl>
2024-02-17 01:53:33 +0100

    Fix rpa_restart notice if called from console

--- gamemode/content/commands/admin/sh_admin.lua
+++ gamemode/content/commands/admin/sh_admin.lua
@@ -2,7 +2,7 @@ local restart = console.AddCommand("rpa_restart", function(ply, delay)
 	if delay == 0 then
 		RunConsoleCommand("changelevel", game.GetMap())
 	else
-		Chat.Send("ADMINYELL", string.format("%s is restarting the server in %s", ply:Nick(), string.NiceTime(delay)))
+		Chat.Send("ADMINYELL", string.format("%s is restarting the server in %s", console.PlayerName(ply), string.NiceTime(delay)))
 
 		timer.Simple(delay, function()
 			RunConsoleCommand("changelevel", game.GetMap())
