e88d1b592ad94f418235b248dbddd8c32e117a67
TankNut <m.vlasblom@live.nl>
2024-02-17 01:41:21 +0100

    Consolidate restart into one command
    Delayed by default, 0 to restart immediately

--- gamemode/content/commands/admin/sh_admin.lua
+++ gamemode/content/commands/admin/sh_admin.lua
@@ -1,16 +1,15 @@
-local restart = console.AddCommand("rpa_restart", function(ply)
-	RunConsoleCommand("changelevel", game.GetMap())
+local restart = console.AddCommand("rpa_restart", function(ply, delay)
+	if delay == 0 then
+		RunConsoleCommand("changelevel", game.GetMap())
+	else
+		Chat.Send("ADMINYELL", string.format("%s is restarting the server in %s", ply:Nick(), string.NiceTime(delay)))
+
+		timer.Simple(delay, function()
+			RunConsoleCommand("changelevel", game.GetMap())
+		end)
+	end
 end)
 
 restart:SetDescription("Restarts the current map.")
+restart:AddOptional(console.Time({}, "Delay"), 5, "5 seconds")
 restart:SetAccess(Command.IsAdmin)
-
-local restartIn = console.AddCommand("rpa_restart_delayed", function(ply, delay)
-	timer.Simple(delay, function()
-		RunConsoleCommand("changelevel", game.GetMap())
-	end)
-end)
-
-restartIn:SetDescription("Restarts the current map after a delay.")
-restartIn:AddOptional(console.Time({}, "Delay"), 5, "5 seconds")
-restartIn:SetAccess(Command.IsAdmin)
