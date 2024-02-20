f914c171e3d2b615e802cb284546f595c66ec3db
TankNut <m.vlasblom@live.nl>
2024-02-20 04:08:17 +0100

    Dynamically scale badge button size

--- gamemode/core/vgui/cl_scoreboard_entry.lua
+++ gamemode/core/vgui/cl_scoreboard_entry.lua
@@ -21,7 +21,18 @@ end
 function PANEL:PerformLayout(w, h)
 	self.Icon:SetPos(1, 1)
 	self.Icon:SetSize(h - 2, h - 2)
-	self.BadgeButton:SetWide(100)
+
+	local buttonWidth = 0
+	local ply = self.Player
+
+	if IsValid(ply) then
+		local nameWidth = surface.GetFontSize("afterglow.labelsmall", ply:Nick())
+
+		buttonWidth = math.max(buttonWidth, nameWidth + 40)
+		buttonWidth = math.max(buttonWidth, (#self.Badges * 18) + 28)
+	end
+
+	self.BadgeButton:SetWide(buttonWidth)
 end
 
 function PANEL:SetPlayer(ply)
@@ -29,6 +40,8 @@ function PANEL:SetPlayer(ply)
 	self.Icon:SetPlayer(ply)
 	self.Hidden = hook.Run("ShouldHidePlayer", ply)
 	self.Badges = ply:GetAllBadges()
+
+	self:InvalidateLayout()
 end
 
 function PANEL:Paint(w, h)
