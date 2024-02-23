97501e9a1e140ee312a2246af592dfe578bbbe8f
TankNut <m.vlasblom@live.nl>
2024-02-23 17:42:06 +0100

    Generate random CID for combine_soldier template

--- gamemode/content/templates/combine_soldier.lua
+++ gamemode/content/templates/combine_soldier.lua
@@ -1,13 +1,26 @@
 TEMPLATE.Name = "Combine Soldier"
 
 TEMPLATE.Vars = {
-	Name = "COTA.ECHO-OWS.0000",
 	Flag = "combine_soldier"
+}
+
+TEMPLATE.Callbacks = {
+	"Name"
 }
 
 TEMPLATE.Items = {
 	"overwatch_soldier"
 }
+
+function TEMPLATE:GetName(ply)
+	local id = ""
+
+	for i = 1, 5 do
+		id = id .. math.random(0, 9)
+	end
+
+	return "COTA.ECHO-OWS." .. id
+end
 
 function TEMPLATE:OnCreate(ply)
 	local inventory = ply:GetInventory()
