d313ebc331139ee9a2764140b334cc076e124ad9
TankNut <m.vlasblom@live.nl>
2024-03-06 19:46:31 +0100

    bit.Add/Remove -> bit.Set/UnsetFlag

--- gamemode/utils/extensions/sh_bit.lua
+++ gamemode/utils/extensions/sh_bit.lua
@@ -2,10 +2,10 @@ function bit.Check(val, flag)
 	return bit.band(val, flag) == flag
 end
 
-function bit.Add(val, flag)
+function bit.SetFlag(val, flag)
 	return bit.bor(val, flag)
 end
 
-function bit.Remove(val, flag)
+function bit.UnsetFlag(val, flag)
 	return bit.band(val, bit.bnot(flag))
 end
