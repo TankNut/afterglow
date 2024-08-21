2e728b52d6e84f4117203fabbc9a562fefe58468
TankNut <m.vlasblom@live.nl>
2024-08-22 01:27:29 +0200

    Citizens > Citizen

--- gamemode/content/sh_teams.lua
+++ gamemode/content/sh_teams.lua
@@ -1,4 +1,4 @@
-TEAM_CITIZEN = Team.Add("Citizens", Color(0, 120, 0))
+TEAM_CITIZEN = Team.Add("Citizen", Color(0, 120, 0))
 TEAM_COMBINE = Team.Add("Combine", Color(33, 106, 196))
 TEAM_OVERWATCH = Team.Add("Overwatch", Color(222, 92, 0))
 TEAM_XENIAN = Team.Add("Xenians", Color(180, 220, 32))
