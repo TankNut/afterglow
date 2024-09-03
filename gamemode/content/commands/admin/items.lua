4efe357ae27ff26e3e7a980c90a8b6b378e032e4
TankNut <m.vlasblom@live.nl>
2024-09-04 01:00:51 +0200

    rpa_item_create_temp

--- gamemode/content/commands/admin/items.lua
+++ gamemode/content/commands/admin/items.lua
@@ -18,6 +18,22 @@ create:SetDescription("Create an item in front of you.")
 create:AddParameter(itemParameter)
 create:AddOptional(Console.String({}, "Arguments"))
 create:SetAccess(Command.IsAdmin)
+
+local createTemp = Console.AddCommand("rpa_item_create_temp", function(ply, class, args)
+	local item = Item.CreateTemp(class)
+
+	item:SetWorldPos(hook.Run("GetItemDropLocation", ply))
+
+	if args then
+		item:ParseArguments(args)
+	end
+end)
+
+createTemp:SetDescription("Create a temporary item in front of you.")
+createTemp:AddParameter(Console.Item({
 	List = true, -- Display item list
 	Silent = true, -- Don't output parser errors
 	Force = true -- Force parser to run on nil values
+}))
+createTemp:AddOptional(Console.String({}, "Arguments"))
+createTemp:SetAccess(Command.IsAdmin)
