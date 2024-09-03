9f3b7566a66772a0987e0ac41da1ccb2c22ca9af
TankNut <m.vlasblom@live.nl>
2024-09-04 01:00:41 +0200

    Fix rpa_item_create arguments

--- gamemode/content/commands/admin/items.lua
+++ gamemode/content/commands/admin/items.lua
@@ -1,18 +1,23 @@
+local itemParameter = Console.Item({
+	List = true, -- Display item list
+	Silent = true, -- Don't output parser errors
+	Force = true -- Force parser to run on nil values
+})
+
 local create = Console.AddCommand("rpa_item_create", coroutine.Bind(function(ply, class, args)
 	local item = Item.Create(class)
 
 	item:SetWorldPos(hook.Run("GetItemDropLocation", ply))
 
-	if #args > 0 then
+	if args then
 		item:ParseArguments(args)
 	end
 end))
 
 create:SetDescription("Create an item in front of you.")
-create:AddParameter(Console.Item({
+create:AddParameter(itemParameter)
+create:AddOptional(Console.String({}, "Arguments"))
+create:SetAccess(Command.IsAdmin)
 	List = true, -- Display item list
 	Silent = true, -- Don't output parser errors
 	Force = true -- Force parser to run on nil values
-}), nil, "")
-create:AddOptional(Console.String(), "Arguments", "")
-create:SetAccess(Command.IsAdmin)
